a303f7641b34dd3814e4e2f78b82af64808a7a35
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-19 19:10:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to.s.capitalize}:"
   holiday.each do |event, supplies|
-    puts " #{events.to.s.split"
+    puts " #{events.to.s.split("_")
 end
 
 def all_holidays_with_bbq(holiday_hash)
